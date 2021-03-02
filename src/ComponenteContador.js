4605d1096356feaccf2e2d9e77c2b9c049e930c5
afelipe072 <andres.rojas.perez@correounivalle.edu.co>
2021-03-02 15:32:33 -0500

    ejercicio en clase

---
+++ src/ComponenteContador.js
@@ -0,0 +1,43 @@
+import React , {useState} from 'react'
+import PropTypes from 'prop-types'
+
+//Andrés Felipe Rojas
+//1741787
+
+
+
+const ComponenteContador = ( {valorInicial} ) =>{
+
+    const [conteo,setConteo]=useState(valorInicial)
+    
+
+
+    return (
+
+        <>
+        <h1>Valor Inicial: {valorInicial}</h1>
+        <br/>
+        <h2>Valor Del Contador: {conteo}</h2>
+
+        <button className="sumar" onClick={(e)=> setConteo(conteo+1)}>
+            Sumar
+        </button>
+
+        <button onClick={(e)=> setConteo(conteo-1)}>
+            Restar            
+        </button>
+
+        <button onClick={(e)=> setConteo(valorInicial)}>
+            Restear            
+        </button>
+
+        </>
+
+    )
+}
+
+ComponenteContador.propTypes ={
+    valorInicial: PropTypes.number.isRequired
+}
+
+export default ComponenteContador
