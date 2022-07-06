ffb1f925acc615e235d00441d715f028b4e724bb
AuraARS <aura.pasmin@omni.pro>
2022-07-05 20:22:20 -0500

    add parcial

---
+++ src/tests/Nombre.test.js
@@ -0,0 +1,15 @@
+import useName from '../hooks/useName'
+describe('Todo relacionado al nombre', () =>{
+
+    test('Prueba saludo con mayuscula', () =>{
+
+        cadena = 'Texto De Prueba';
+        cadena = cadena.toUpperCase();
+        document.write(cadena);
+    })
+    test('Prueba saludo con tamano', () =>{
+
+        let browserType = 'mozilla';
+        browserType.length;
+    })
+})
