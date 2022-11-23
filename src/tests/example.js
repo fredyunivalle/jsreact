6dc0a65a55ceb2c4642d20e59675fafaa197c090
jhan <jhan.perez@correounivalle.edu.co>
2022-11-22 20:04:23 -0500

    Test divisor, number 10 as example

---
+++ src/tests/example.js
@@ -0,0 +1,18 @@
+const findDivisor = (number) => {
+
+    const my_array = [];
+    let initial = 0;
+
+    while (initial<=number) {
+        if (initial % 2 != 0 && initial % 3 != 0) {
+            my_array.push(initial);
+            initial +=1;
+        } else {
+            initial += 1;
+        }
+    }
+
+    return my_array;
+}
+
+console.log(findDivisor(10))
